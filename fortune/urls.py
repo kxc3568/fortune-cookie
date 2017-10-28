b0b9377da2b24851ce207c17de91d205ae08b926
Kevin Chen <kchen3568@gmail.com>
2017-10-28 17:13:53 -0500

    form

--- fortune/urls.py
+++ fortune/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.HomeView)
+	url(r'^$', views.HomeView),
+	url(r'^fortune/$', views.FortuneView),
 ]
--- fortune/views.py
+++ fortune/views.py
@@ -4,3 +4,6 @@ from .utils import getTimeline
 # Create your views here.
 def HomeView(request):
 	return render(request, 'home.html', {'tweets': getTimeline('dhruvak_')})
+
+def FortuneView(request):
+	return render(request, 'fortune.html')
