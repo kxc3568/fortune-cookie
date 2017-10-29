2e465a470fc299ea438f9b9f9775d9d96aaf8214
Kevin Chen <kchen3568@gmail.com>
2017-10-28 19:48:27 -0500

    link page

--- fortune/forms.py
+++ fortune/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class UserForm(forms.Form):
 	username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'placeholder': 'Enter your Username'}))
+
+	def clean_username(self):
+		data = self.cleaned_data['username']
+		return data
